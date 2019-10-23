8da77002db0904fd172e5bcf7d4bce98ca8d67ca
Skyler Torian <skymanat7@yahoo.com>
2019-10-23 18:12:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -55,6 +55,7 @@ end
 def using_delete(instructors, no_offense_steven)
 instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
 no_offense_steven = instructors.delete("Steven")
+puts instructors
 end
 
 def using_delete_at(famous_robots, integer)
